3a2903fa216220ae5818c7c2eda480fdf7da2ece
sreesakthi.p.p <sakthisreesakthi61@gmail.com>
2025-04-30 03:44:48 +0530

    cors changed

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ const { bookingRouter } = require("./routes/bookingRoutes");
 
 dbConnection()
 app.use(cors({
-    origin:"https://movie-ticket-website-mac1.vercel.app/",credentials:true
+    origin:"https://movie-ticket-website-mac1.vercel.app",credentials:true
 }));
 app.get("/", (req, res) => {
     res.json("server started");
